ef8e09bf378f66dc21ad34b0c922bb818d4edf29
jakesb27 <jake.boden27@gmail.com>
2023-06-02 14:38:56 -0700

    created initial flask app and updated gitignore

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/<movie>")
+def index_get(movie):
+    """Homepage GET request"""
+
+    return f"{escape(movie)} you want to match"
+
+
+app.run(debug=True)
